3f1b8ea0791a39b68b1e4262e5c58cb5621b2f13
dswsd <babdoy123@gmail.com>
2025-06-18 10:17:10 +0900

    완료

--- src/main/java/com/example/boot/controller/ExController.java
+++ src/main/java/com/example/boot/controller/ExController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ExController {
-    @GetMapping("/index")
+    @GetMapping({"/", "/index"}) // 클라이언트가 url요청한 주소
         public String index(){
-            return "index";
+            /* 스프링 부트의 viewResolver가 가지고 있는 것
+            *  prefix : 프로젝트명(애플리케이션명) "boot/src/main/resources/templates/"
+            *  suffix : ".html"
+            *  "boot/src/main/resources/templates/" + "index" + ".html"
+            * */
+            return "index"; // 클라이언트에게 보내줄 페이지 주소
+    }
+    @GetMapping("/joinform")
+        public String joinform(){
+        return "joinform";
     }
 }
