38d82354d2fa220c1f64bfad754ea369ead195a0
dswsd <babdoy123@gmail.com>
2025-06-18 09:55:33 +0900

    완료

--- src/main/java/com/example/boot/controller/ExController.java
+++ src/main/java/com/example/boot/controller/ExController.java
@@ -1,8 +1,12 @@
 package com.example.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ExController {
-
+    @GetMapping("/index")
+        public String index(){
+            return "index";
+    }
 }
