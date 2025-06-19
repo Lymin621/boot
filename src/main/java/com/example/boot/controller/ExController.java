dc58dc70e2758d409a4855c5163777d4138dd8ee
dswsd <babdoy123@gmail.com>
2025-06-19 18:01:35 +0900

    완료

--- src/main/java/com/example/boot/controller/ExController.java
+++ src/main/java/com/example/boot/controller/ExController.java
@@ -2,6 +2,7 @@ package com.example.boot.controller;
 
 import com.example.boot.svc.UserSvc;
 import com.example.boot.vo.User;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,23 +20,24 @@ public class ExController {
     UserSvc svc;
 
     @GetMapping({"/", "/index"}) // 클라이언트가 url요청한 주소
-        public String index(){
-            /* 스프링 부트의 viewResolver가 가지고 있는 것
-            *  prefix : 프로젝트명(애플리케이션명) "boot/src/main/resources/templates/"
-            *  suffix : ".html"
-            *  "boot/src/main/resources/templates/" + "index" + ".html"
-            * */
-            return "index"; // 클라이언트에게 보내줄 페이지 주소
+    public String index() {
+        /* 스프링 부트의 viewResolver가 가지고 있는 것
+         *  prefix : 프로젝트명(애플리케이션명) "boot/src/main/resources/templates/"
+         *  suffix : ".html"
+         *  "boot/src/main/resources/templates/" + "index" + ".html"
+         * */
+        return "index"; // 클라이언트에게 보내줄 페이지 주소
     }
+
     //회원가입 폼 페이지 열기
     @GetMapping("/joinform")
-        public String joinform(){
+    public String joinform() {
         return "joinform";
     }
 
     //회원가입 데이터베이스에 넣기
     @PostMapping("/joinform") // 동일한 메소드 방식의 동일한 url패턴이 2개 이상 존재하면 안됨.
-        public String joinform(User user){
+    public String joinform(User user) {
         System.out.println(user);
         svc.joinUs(user);
         return "joinresult";
@@ -59,30 +61,46 @@ public class ExController {
             return "login";
         }
     }
+
     //로그인 창
-    @RequestMapping(value ="/userlogin", method=RequestMethod.GET)
-        public String userlogin(){
+    @RequestMapping(value = "/userlogin", method = RequestMethod.GET)
+    public String userlogin() {
         return "login";
     }
-    @RequestMapping(value ="/userlogin", method=RequestMethod.POST)
-        public String userlogin(User user, HttpSession session){
-        System.out.println("로그인의 user: "+user);
+
+    @RequestMapping(value = "/userlogin", method = RequestMethod.POST)
+    public String userlogin(User user, HttpSession session) {
+        System.out.println("로그인의 user: " + user);
         User vo = svc.userLogin(user);
-        if(vo != null){
+        if (vo != null) {
             System.out.println("로그인 성공");
-            session.setAttribute("loginid",user.getUid());
+            session.setAttribute("loginid", user.getUid());
             return "index";
-        }else{
+        } else {
             System.out.println("로그인 실패");
             return "login";
         }
 
     }
+
     //로그아웃 처리하기
     @GetMapping("/logout")
-    public String logout(HttpSession session){
+    public String logout(HttpSession session) {
         System.out.println("로그아웃 성공");
         session.invalidate();
+        return "index";
+    }
+
+    @GetMapping("/userdelete")
+    public String userDeleteForm() {
+        return "userdelete";
+    }
+
+    @PostMapping("/userDelete")
+    public String userDelete(HttpServletRequest request) {
+        String uid = (String) request.getSession().getAttribute("loginid");
+        svc.userDelete(uid);
+        request.getSession().invalidate();
         return "index";
     }
 
