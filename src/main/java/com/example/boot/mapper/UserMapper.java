639f00d6151d7c476546c0f77ab8ff15451c0ed2
dswsd <babdoy123@gmail.com>
2025-06-19 10:58:49 +0900

    완료

--- src/main/java/com/example/boot/mapper/UserMapper.java
+++ src/main/java/com/example/boot/mapper/UserMapper.java
@@ -5,6 +5,16 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
+    // 회원 전체 목록 조회
     User userList();
+    //회원 가입
     int joinUser(User user);
+    //회원 정보 수정
+    int userUpdate(User user);
+    //회원 탈퇴
+    int userDelete(String uid);
+    //회원 일부 목록 죄회
+    User userSelList(User user);
+    //나의 정보 보기
+    User userOne(String user);
 }
--- src/main/java/com/example/boot/svc/UserSvc.java
+++ src/main/java/com/example/boot/svc/UserSvc.java
@@ -11,7 +11,30 @@ public class UserSvc {
     @Autowired
     UserMapper mapper;
 
+    //회원 전체 목록 보기
+    public User userList(User user){
+        return mapper.userList();
+    }
+
+    //회원 가입 관련
     public int joinUs(User user){
         return mapper.joinUser(user);
+    }
+
+    //    회원 정보 수정
+    public int userDelete(String uid){
+        return mapper.userDelete(uid);
+    }
+    //    회원 탈퇴
+    public int userUpdate(User user){
+        return mapper.userUpdate(user);
+    }
+    //    회원 일부 목록
+    public User userSelList(User user){
+        return mapper.userSelList(user);
+    }
+    //    나의 정보 보기
+    public User userOne(String uid){
+        return mapper.userOne(uid);
     }
 }
